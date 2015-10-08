8b787c22fa815c14b70bd46949fdad0e9639c7b1
SVN-Git Migration <python-modules-team@lists.alioth.debian.org>
2015-10-08 12:33:41 -0700

    Imported Upstream version 0.5.6

---
+++ netfilter/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007-2009 Bolloré telecom
+# See AUTHORS file for a full list of contributors.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+python-netfilter
+Bolloré Telecom
+
+@author: Jeremy Lainé <jeremy.laine@bolloretelecom.eu>
+"""
+__docformat__ = 'epytext en'
+
+__version__ = '0.5.6'
+__url__ = 'http://opensource.bolloretelecom.eu/projects/python-netfilter'
+__copyright__ = '(C) 2007-2009 Bolloré Telecom'
+__license__ = 'GPLv3'
+__license_long__ = """
+ Copyright (C) 2007-2009 Bolloré Telecom
+ See AUTHORS file for a full list of contributors."""
+
