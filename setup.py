8c90fbc9ef8d65e5b26ffbaed0aa6723d025a59e
Jeremy Lainé <jeremy.laine@m4x.org>
2015-10-22 17:00:00 +0200

    Imported Upstream version 0.6.3

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup
 
 setup(
-    name='python-netfilter',
-    version='0.6.1',
+    name='netfilter',
+    version='0.6.3',
     description='Python modules for manipulating netfilter rules',
     url='https://github.com/jlaine/python-netfilter',
     author='Jeremy Laine',
@@ -14,7 +14,15 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Networking :: Firewalls',
+        'Topic :: System :: Systems Administration',
     ],
     test_suite='tests',
 )
